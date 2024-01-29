07c8c4eac78f3c4621c4dfac3cff6f2924b67227
kotesh-arya <koteswarraomudila@gmail.com>
2024-01-29 14:36:18 +0530

    ADD: useEffect and The crucial cleanup function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import { useState, useEffect } from "react";
+
+function Playground() {
+  const [text, setText] = useState("abc");
+
+  useEffect(() => {
+    function onTimeout() {
+      console.log("⏰ " + text);
+    }
+
+    console.log('🔵 Schedule "' + text + '" log');
+    const timeoutId = setTimeout(onTimeout, 3000);
+
+    return () => {
+      console.log(
+        '🟡 Cancel "' + text + '" log',
+        "Iam a clean-up and I run before every next render and during unmounting of this COMPONENT"
+      );
+      clearTimeout(timeoutId);
+    };
+  }, [text]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label>
+        What to log:{" "}
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+      </label>
+      <h1>{text}</h1>
+    </>
   );
 }
 
-export default App;
+export default function App() {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <button onClick={() => setShow(!show)}>
+        {show ? "Unmount" : "Mount"} the component
+      </button>
+      {show && <hr />}
+      {show && <Playground />}
+    </>
+  );
+}
